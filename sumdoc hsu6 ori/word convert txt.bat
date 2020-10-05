4416fd2efce21d6d740c00154e09905f1e914559
attilax@gmail.cm <attilax@gmail.cm>
2020-10-05 09:15:27 +0800

    ---

---
+++ sumdoc hsu6 ori/word convert txt.bat
@@ -0,0 +1,4 @@
+D:\doctotext\doctotext.exe  "C:\Users\Administrator\OneDrive\桌面\prjs hsu6\2020docx\sumdoc hsu6\Atitit 稳定性提升总结kok案例.docx"
+
+
+for %I in ("C:\Users\Administrator\OneDrive\桌面\prjs hsu6\2020docx\sumdoc hsu6\*.*") do D:\doctotext\doctotext.exe "%I" >"%I.txt"
